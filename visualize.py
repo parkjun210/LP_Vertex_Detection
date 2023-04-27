1fdb8699bfa029c202c2421ff348a33c8ab13f9f
parkjun210 <68048434+parkjun210@users.noreply.github.com>
2023-04-27 19:26:01 +0900

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -13,24 +13,27 @@ def visualize():
     
     args = parse_args()
 
-    GPU_NUM = args.gpu_num
-    NUM_WORKERS = args.num_workers
-
-    EXP_DIR = os.path.join('experiments', args.exp_name)
-    WEIGHTS = args.weights
-
-    SAVE_DIR = os.path.join(args.save_dir, args.exp_name)
-    SAVE_IMG = args.save_img
+    #### Argument ####
+    
+    GPU_NUM = 0
+    # ckpt 파일 경로
+    WEIGHT_PATH     = args.weights_path
+    # data 폴더 안 inferece할 폴더 경로
+    INFERENCE_DIR   = os.path.join('data', args.inference_dir)
+    # inference 결과가 담길 폴더 경로
+    SAVE_DIR        = os.path.join('result', args.inference_dir)
+    # Inference result image 저장 여부
+    SAVE_IMG_FLAG   = args.save_img_flag
 
 
     # Set up Dataset
-    test_dataset = Visual_Dataset(args, 'test')
+    test_dataset = Visual_Dataset(args, INFERENCE_DIR)
 
     test_dataloader = DataLoader(
         dataset=test_dataset,
         batch_size=1,
         shuffle=False,
-        num_workers=NUM_WORKERS,
+        num_workers=args.num_workers,
         pin_memory=True,
         drop_last=False
     )
@@ -38,23 +41,20 @@ def visualize():
     # Set up Network
     network = LPDetectionNet(args)
 
-
     # Set up GPU
     os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
     os.environ['CUDA_VISIBLE_DEVICES'] = str(GPU_NUM)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
     # Move the network to GPU if possible
     if torch.cuda.is_available():
         network.to(device)
 
-
-    # ckpt = torch.load(os.path.join('experiments', 'exp_4', 'ckpt.pth'))
-    ckpt = torch.load(os.path.join(EXP_DIR, WEIGHTS))
+    ckpt = torch.load(WEIGHT_PATH)
     network.load_state_dict(ckpt['network'])
 
     with torch.no_grad():
-        inference(network, test_dataloader, device, save_dir = SAVE_DIR, save_img = SAVE_IMG)
+        inference(network, test_dataloader, device, save_dir = SAVE_DIR, save_img_flag = SAVE_IMG_FLAG)
 
 
 if __name__ == '__main__':
